17277c53c2143f51105183474becfa1ea8cadc87
Evgeniy <e.mechsheryakov@gmail.com>
2020-03-16 10:14:51 +0100

    CSS refactoring - CurrenciesList component Testing - CurrenciesList component

--- src/components/CurrenciesList/CurrenciesList.js
+++ src/components/CurrenciesList/CurrenciesList.js
@@ -1,10 +1,11 @@
 import React, {useEffect} from "react";
 import arrowDown from "../../assets/down-arrow.svg";
+import './Currencies.css';
 
 const CurrenciesList = (props) => {
     return (
         <div className="currencies">
-            <select className="currencies__dropdown" value={props.selectedWallet} onChange={props.changedOption}>
+            <select className="currencies__dropdown" defaultValue={props.selectedWallet} onChange={props.changedOption}>
                 {props.children}
             </select>
             <img className="currencies__arrow-down" src={arrowDown} alt="arrow-down"/>
---
+++ src/components/CurrenciesList/CurrenciesList.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import CurrenciesList from "./CurrenciesList";
+
+let container = null;
+const input = () => container.querySelector('.currencies__dropdown');
+beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+it("renders CurrenciesList with props values", () => {
+    render(<CurrenciesList selectedWallet={'usd'}><option value={'usd'}></option></CurrenciesList>, container);
+    expect(input().value).toBe('usd');
+});
